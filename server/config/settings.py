c327e651cd0a17b947a71e6359908434ce151e73
Ayame <ayame.space@gmail.com>
2021-02-08 05:11:44 +0900

    スロットリングを追加

--- server/config/settings.py
+++ server/config/settings.py
@@ -135,7 +135,15 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/minute',
+        'user': '1000/hour',
+    }
 }
 
 SIMPLE_JWT = {
