f13f1273b4430acaf239ccd11ec9a70b3acef905
Ayame <ayame.space@gmail.com>
2021-02-08 09:57:46 +0900

    django STATIC_ROOTの設定に対応

--- server/config/settings.py
+++ server/config/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+if os.environ.get("STATIC_ROOT"):
+    STATIC_ROOT = os.environ.get("STATIC_ROOT")
 
 
 REST_FRAMEWORK = {
